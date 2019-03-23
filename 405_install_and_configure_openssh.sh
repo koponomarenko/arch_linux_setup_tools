897cb3eccc4776a9a0733e1bce980f5b04f2e2c5
Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>
2019-03-23 22:41:25 +0200

    Add github ssh key generation

--- 405_install_and_configure_openssh.sh
+++ 405_install_and_configure_openssh.sh
@@ -6,3 +6,16 @@ pkgs=(openssh)
 . common_install_and_configure.sh
 
 install_pkgs
+
+github_com_key="${user_dir}/.ssh/id_ed25519_github"
+cmd_do ssh-keygen -t ed25519 -N \"\" -C "$(whoami)@$(hostname)-$(date -I)" -f ${github_com_key}
+
+ssh_config="${user_dir}/.ssh/config"
+cmd_do 'cat >${ssh_config} <<EOF
+Host github.com
+   IdentitiesOnly yes
+   IdentityFile ${github_com_key}
+EOF'
+
+cmd_do chown -c ${user}:${user} "${ssh_config}"
+cmd_do chmod 600 "${ssh_config}"

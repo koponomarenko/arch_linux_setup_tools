f220e2b0dabf23657256066f3781667d722e7076
Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>
2019-03-24 11:21:52 +0200

    Add firefox-wl alias for FF-Wayland

--- 510_install_and_configure_firefox.sh
+++ 510_install_and_configure_firefox.sh
@@ -6,5 +6,13 @@ pkgs=(firefox)
 . common_install_and_configure.sh
 
 install_pkgs
-#TODO: configure it
-#copy_config_file "${config_dir}/" "${user_dir}/."
+
+alias_dest_file="${user_dir}/.bashrc"
+firefox_wl_alias='alias firefox-wl="GDK_BACKEND=wayland firefox"'
+if ! grep -q "${firefox_wl_alias}" ${alias_dest_file}; then
+    cmd_do 'cat >>${alias_dest_file} <<EOF
+
+${firefox_wl_alias}
+
+EOF'
+fi

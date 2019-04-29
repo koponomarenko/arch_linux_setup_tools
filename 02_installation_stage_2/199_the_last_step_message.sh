530d07cf32c8469d865a2958cb5ae655c5e95e6a
Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>
2019-04-29 14:54:48 +0300

    Add "installation_stage_2 completed." message

--- 02_installation_stage_2/199_the_last_step_message.sh
+++ 02_installation_stage_2/199_the_last_step_message.sh
@@ -3,6 +3,8 @@
 . ${root_dir}/common_helpers/functions.sh
 
 echo ""
+echo "installation_stage_2 completed."
+echo ""
 echo "Exit the chroot environment by typing exit or pressing Ctrl+D."
 echo "Optionally manually unmount all the partitions with 'umount -R /mnt'."
 echo "Restart the machine by typing 'reboot'."

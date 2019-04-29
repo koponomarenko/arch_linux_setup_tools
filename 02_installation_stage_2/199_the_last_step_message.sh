45b3b48bd24489b72e9d98b8ac8ccee4b1a71a6d
Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>
2019-04-29 11:42:41 +0300

    Add 199_the_last_step_message.sh

---
+++ 02_installation_stage_2/199_the_last_step_message.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+. ${root_dir}/common_helpers/functions.sh
+
+echo ""
+echo "Exit the chroot environment by typing exit or pressing Ctrl+D."
+echo "Optionally manually unmount all the partitions with 'umount -R /mnt'."
+echo "Restart the machine by typing 'reboot'."

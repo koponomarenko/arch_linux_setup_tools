d669344ecd7bdec130e6b287f05920795a1ab637
Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>
2019-05-02 22:24:06 +0300

    Fix 405_install_and_configure_openssh.sh

--- 03_configuration_stage_1/405_install_and_configure_openssh.sh
+++ 03_configuration_stage_1/405_install_and_configure_openssh.sh
@@ -11,6 +11,7 @@ ssh_dir="${user_dir}/.ssh"
 mk_dest_config_dir ${ssh_dir}
 github_com_key="${ssh_dir}/id_ed25519_github"
 cmd_do ssh-keygen -t ed25519 -N \"\" -C "$(whoami)@$(hostname)-$(date -I)" -f ${github_com_key}
+cmd_do chown ${user}:${user} "${github_com_key}" "${github_com_key}.pub"
 
 ssh_config="${ssh_dir}/config"
 cmd_do 'cat >${ssh_config} <<EOF

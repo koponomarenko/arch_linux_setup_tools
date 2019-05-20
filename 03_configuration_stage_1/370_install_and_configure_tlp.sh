e0897e3854927a3ccfac28609138844a3cbf7eb8
Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>
2019-05-20 23:27:30 +0300

    Add TLP

---
+++ 03_configuration_stage_1/370_install_and_configure_tlp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. ${root_dir}/common_helpers/functions.sh
+. ${root_dir}/common_helpers/common_install_and_configure.sh
+
+pkgs=(
+    tlp
+    tlp-rdw # NetworkManager is required for this one
+)
+
+install_pkgs "${pkgs[@]}"
+
+systemctl enable tlp.service
+systemctl start tlp.service
+
+systemctl enable tlp-sleep.service
+systemctl start tlp-sleep.service
+
+systemctl enable NetworkManager-dispatcher.service
+
+systemctl mask systemd-rfkill.service
+systemctl mask systemd-rfkill.socket

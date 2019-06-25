d2855ea69b2cd1a9a8341c7a64be3b2df89b982c
Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>
2019-06-25 14:54:20 +0300

    Clean up termite config script

--- 04_sway_wm/510_install_and_configure_termite.sh
+++ 04_sway_wm/510_install_and_configure_termite.sh
@@ -7,6 +7,5 @@ pkgs=(termite)
 
 install_pkgs "${pkgs[@]}"
 
-main_custom_config ${user_config_dir}/termite
 mk_dest_config_dir ${user_config_dir}/termite
 copy_config_file "${sway_wm_config_dir}/termite/config" "${user_config_dir}/termite/config"

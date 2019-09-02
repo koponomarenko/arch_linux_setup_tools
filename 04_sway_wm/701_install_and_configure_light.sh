1990c8eae72ef77cb06c867fab00c29318441d67
Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>
2019-09-02 22:15:05 +0300

    Git permissions to light to set brightness
    Signed-off-by: Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>

--- 04_sway_wm/701_install_and_configure_light.sh
+++ 04_sway_wm/701_install_and_configure_light.sh
@@ -13,3 +13,6 @@ install_pkgs "${pkgs[@]}"
 screen_brightness="/usr/local/bin/screen_brightness.sh"
 cmd_do cp ${root_dir}/tools/screen_brightness.sh ${screen_brightness}
 cmd_do chmod +x ${screen_brightness}
+
+# Give permissions to "light"
+#setcap cap_dac_override+eip /usr/bin/light
